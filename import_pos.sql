7a0302c9c4b3b4bcc65f961d8ff1a25819d4e06a
Nigmatullin Gleb <93813322+nigmatullin71@users.noreply.github.com>
2024-06-19 01:21:14 +0300

    Update import_pos.sql

--- import_pos.sql
+++ import_pos.sql
@@ -14,11 +14,34 @@ INSERT INTO position (earthpos, sunPosition, moonPosition) VALUES
 ('EarthPos4', 'SunPos4', 'MoonPos4'),
 ('EarthPos5', 'SunPos5', 'MoonPos5');
 
+-- Создание второй таблицы details с автоинкрементом для id и внешним ключом position_id
+CREATE TABLE details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    position_id INT,
+    details_id INT,
+    description VARCHAR(255),
+    FOREIGN KEY (position_id) REFERENCES position(id)
+);
+
+-- Вставка данных в таблицу details с указанием внешнего ключа position_id
+INSERT INTO details (position_id, details_id description) VALUES
+(1, 'Description1'),
+(2, 'Description2'),
+(3, 'Description3'),
+(4, 'Description4'),
+(5, 'Description5');
+
+-- Изменение разделителя перед созданием процедуры
 DELIMITER //
-CREATE PROCEDURE JoinTables(IN table1 VARCHAR(50), IN table2 VARCHAR(50))
+
+-- Создание хранимой процедуры JoinTables с объединением таблиц
+CREATE PROCEDURE JoinTables()
 BEGIN
-    SET @query = CONCAT('SELECT * FROM ', table1, ' JOIN ', table2, ' ON ', table1, '.id = ', table2, '.', table1, '_id');
-    PREPARE stmt FROM @query;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
+    SELECT p.id, p.earthpos, p.sunPosition, p.moonPosition, d.position_id d.details_id
+    FROM position p
+    JOIN details d ON p.id = d.position_id;
+END //
+
+-- Возвращение разделителя к точке с запятой
 DELIMITER ;
+
